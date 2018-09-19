c420fbf2bccc1aa4da906cda4552296a891fd9b3
iednoraljuk <ivan.ednoraliuk@mail.ru>
2018-09-19 16:29:11 +0300

    fix

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import ExampleComponent from "react-scaling-scroll";
+import ExampleComponent from 'react-scaling-scroll';
 
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<ExampleComponent elements={[
-					<p key={1}>1</p>,
-					<p key={2}>2</p>,
-					<p key={3}>3</p>
-				]} />
-			</div>
-		);
-  }
+    render() {
+        return (
+            <div>
+                <ExampleComponent
+                    elements={[
+                        <p key={1}>1</p>,
+                        <p key={2}>2</p>,
+                        <p key={3}>3</p>
+                    ]}
+                />
+            </div>
+        );
+    }
 }
